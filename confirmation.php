c6fd854e141c47ab0e50eaa80cd575f271cb5ff6
wakanomanimani <58735953+wakanomanimani@users.noreply.github.com>
2019-12-13 22:18:16 +0900

    Update confirmation.php

--- confirmation.php
+++ confirmation.php
@@ -2,7 +2,7 @@
     <h2>確認画面</h2>
     <table border="1">
       <tr>
-        <td>名前</td>
+        <td>お名前</td>
         <td><?php echo $_POST["name"]; ?></td>
       </tr>
       <tr>
@@ -10,8 +10,8 @@
         <td><?php echo $_POST["mail"]; ?></td>
       </tr>
       <tr>
-        <td>問い合わせ内容</td>
-        <td><?php echo $_POST["content"]; ?></td>
+        <td>コメント</td>
+        <td><?php echo $_POST["comment"]; ?></td>
       </tr>
     </table>
     <input type="submit" value="送信"/>
@@ -22,5 +22,5 @@
   session_start() ;
   $_SESSION["name"] = $_POST["name"];
   $_SESSION["mail"] = $_POST["mail"];
-  $_SESSION["content"] = $_POST["content"];
+  $_SESSION["comment"] = $_POST["comment"];
   ?>
