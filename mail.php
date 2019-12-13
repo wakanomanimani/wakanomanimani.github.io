6816fd815ffe06163438c623901c7b787750f19e
wakanomanimani <58735953+wakanomanimani@users.noreply.github.com>
2019-12-13 22:32:22 +0900

    Update mail.php

--- mail.php
+++ mail.php
@@ -2,7 +2,7 @@
   mb_language("Japanese");
   mb_internal_encoding("UTF-8");
   session_start();
-  $to = wakanomanimani.google.com;
+  $to = "wakanomanimani@google.com";
   $name = htmlspecialchars($_SESSION['name']);
   $mail = htmlspecialchars($_SESSION['mail']);
   $comment = htmlspecialchars($_SESSION['comment']);
@@ -16,4 +16,4 @@
   <?php else : ?>
     <p>メールの送信が失敗しました</p>
   <?php endif ; ?>
-  <a href="index.html">戻る</a>
+  <a href="https://wakanomanimani.github.io/index.html">戻る</a>
